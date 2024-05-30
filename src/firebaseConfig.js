b88eeebe4677f24eba8e066852c2a4dfa999d533
zap3420 <98231455+zap3420@users.noreply.github.com>
2024-05-30 17:12:45 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBBhK06VdwPfoKYh5nMYVmbI7yWWBwr3wY",
-    authDomain: "chatzap-d58a4.firebaseapp.com",
-    projectId: "chatzap-d58a4",
-    storageBucket: "chatzap-d58a4.appspot.com",
-    messagingSenderId: "217888563749",
-    appId: "1:217888563749:web:ca7eb99f9053f1044f95cc"
-  };
-  
-  export default firebaseConfig;
-  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+export default firebaseConfig;
